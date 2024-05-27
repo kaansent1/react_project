90c991cd0322c9d63caeb442271f25a58a00f0fb
ksentuerk1 <ksentuerk@progeek.de>
2024-05-27 14:01:50 +0200

    creditpage design modified

--- react_test/src/pages/CreditsPage.tsx
+++ react_test/src/pages/CreditsPage.tsx
@@ -6,23 +6,50 @@ import {useClient} from "../context/ClientContext.tsx";
 import {useNavigate} from "react-router-dom";
 import BackButton from "../components/BackButton.tsx";
 
+import srlogo from "../assets/logo.svg";
+import reactLogo from "../assets/react.svg";
+import sqlLogo from "../assets/sqlLogo.png";
+import axiosLogo from "../assets/axiosLogo.png"
+import muiLogo from "../assets/muiLogo.png"
+import kotlinLogo from "../assets/kotlinLogo.png"
 
 const CreditsPage: React.FC = () => {
     const {client} = useClient();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     useEffect(() => {
-        if (client.userId == 0) {
-            navigate("/")
+        if (client.userId === 0) {
+            navigate("/");
         }
     }, [client.userId, navigate]);
+
     return (
         <div>
             <Header/>
-            <div className="credits-container">
-                <span className="contributors">Kaan Sentürk </span>
-            </div>
             <BackButton/>
+            <div className="credits-container">
+                <h1>Kaan Sentürk</h1>
+                <div className="logo-container">
+                    <a href="https://github.com/kaansenturk/react_project">
+                        <img src={srlogo} alt="srLogo" className="emblem"/>
+                    </a>
+                    <a href="https://reactjs.org">
+                        <img src={reactLogo} alt="React Logo" className="react-logo"/>
+                    </a>
+                    <a href="https://www.postgresql.org/">
+                        <img src={sqlLogo} alt="SQL Logo" className="react-logo"/>
+                    </a>
+                    <a href="https://axios-http.com/">
+                        <img src={axiosLogo} alt="Axios Logo" className="axios-logo"/>
+                    </a>
+                    <a href="https://mui.com/">
+                        <img src={muiLogo} alt="SQL Logo" className="react-logo"/>
+                    </a>
+                    <a href="https://www.postgresql.org/">
+                        <img src={kotlinLogo} alt="SQL Logo" className="react-logo"/>
+                    </a>
+                </div>
+            </div>
             <Footer/>
         </div>
     );
